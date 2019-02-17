c0bb4a782eddbad182afbac6b5417a48ab6e6984
Andrew Holway <andrew.holway@brickblock.io>
2019-02-17 14:59:10 +0100

    fixed routes

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 import psycopg2
+import jsonify
+import pprint
 
 database_env = {
-	"user": os.environ['PG_USERNAME'],
-	"password": os.environ['PG_PASSWORD'],
-	"host": os.environ['PG_HOST'],
-	"database": os.environ['PG_DATABASE']
+    "user": os.environ['PG_USERNAME'],
+    "password": os.environ['PG_PASSWORD'],
+    "host": os.environ['PG_HOST'],
+    "database": os.environ['PG_DATABASE']
 }
 
 
@@ -15,17 +17,31 @@ DB_URL = 'postgresql+psycopg2://{user}:{password}@{host}/{database}'.format(**da
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 db = SQLAlchemy(app)
-db.create_all()
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-
     def __repr__(self):
         return '<User %r>' % self.username
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+
+@app.route("/api/user/<username>")
+def hello(username):
+    admin = User(username=username, email=username + '@meowtron.com')
+    db.session.add(admin)
+    db.session.commit()
+    print(User.query.all())
+    return 'OK'
+
+@app.route("/healthz")
+def healthz():
+    return 'OK'
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run()
+
+
