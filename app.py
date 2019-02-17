9c07578c7f88c7ba305e117abce3ea0296c611cc
Andrew Holway <andrew.holway@brickblock.io>
2019-02-17 16:40:21 +0100

    not sure what this is accomplishing

--- app.py
+++ app.py
@@ -26,15 +26,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 db = SQLAlchemy(app)
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     def __repr__(self):
         return '<User %r>' % self.username
-
-
 
 @app.route("/api/user/<username>")
 def hello(username):
