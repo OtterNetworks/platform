541a544f10735f552898034c64a8ffdb938856f8
Andrew Holway <andrew.holway@brickblock.io>
2019-02-17 20:14:28 +0100

    updated start script

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Create the database
 python createDB.py
 
+# Run the latest migrations
+flask db upgrade
+
+# Start the server
 /usr/local/bin/gunicorn --config /app/gunicorn.conf app:app
